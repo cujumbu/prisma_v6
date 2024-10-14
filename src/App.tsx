db272ff04cdd7d1998ab972bcd44fc0c89dd2cd2
cujumbu <nethandelskompagniet@gmail.com>
2024-10-14 15:06:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Warranty Claims Portal</h1>
+      <p>If you can see this, React is working!</p>
+    </div>
+  );
+}
+
+export default App;
